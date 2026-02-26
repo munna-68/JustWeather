4cf561b9bca038af456366cc90662c84c69da4ca
munna-68 <mahamudmunna68@gmail.com>
2026-02-27 00:54:36 +0600

    making the webapp responsive for mobile use this was way complex then i though tbh

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import "./styles/style.css";
 import { print } from "./js/module";
 
 console.log(print);
+
+// Theme toggle card expand/collapse
+document.addEventListener("DOMContentLoaded", () => {
+  const themeCard = document.querySelector(".theme-toggle-card");
+  const cardHeader = themeCard?.querySelector(".card-header");
+
+  if (cardHeader && themeCard) {
+    cardHeader.addEventListener("click", () => {
+      themeCard.classList.toggle("expanded");
+    });
+  }
+});
