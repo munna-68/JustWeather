6fd8480c309fd03e76d3e23f8e152fb6f3da3f56
munna-68 <mahamudmunna68@gmail.com>
2026-03-18 16:52:21 +0600

    writing the fetch api to get all the weather data needed for the app

--- src/js/fetchWeather.js
+++ src/js/fetchWeather.js
@@ -1,3 +1,35 @@
 import { getWeatherDomNodes } from "./renderWeather";
 
 export const print = () => getWeatherDomNodes();
+const API_KEY = "S3LZG586WWP9JBSCNR433MST6";
+const location = "New York, NY";
+
+async function getWeather() {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=us&include=current&elements=datetime,temp,feelslike,humidity,cloudcover,uvindex,precip,precipprob,conditions,icon&key=${API_KEY}`,
+      {
+        method: "GET",
+        headers: {},
+      },
+    );
+
+    if (!response.ok) {
+      throw response;
+    }
+
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (errorResponse) {
+    if (errorResponse.text) {
+      const errorMessage = await errorResponse.text();
+      console.error("Error details:", errorMessage);
+    } else {
+      console.error("Unknown error occurred.");
+    }
+  }
+}
+
+getWeather().then((data) => {
+  console.log(data.currentConditions);
+});
